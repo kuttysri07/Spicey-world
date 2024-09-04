282e8727a1132654eafc018034f1e301e3177f45
Sriram <sriram29072002@gmail.com>
2024-09-04 10:46:58 +0530

    updated security

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import Nav from './components/nav/Nav.jsx'
 import Hero from './components/hero/Hero.jsx'
 import Special from './components/Specialdish/Special.jsx'
 import Chef from './components/Chef/Chef.jsx'
 import Contact from './components/contact/Contact.jsx'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Dishes from './components/Dishes/Dishes.jsx'
 import Login from './components/Login/login.jsx'
+import { auth } from './components/firebase.js'
 
 const App = () => {
+  const [user ,setUser] = useState();
+  useEffect(()=>{
+    auth.onAuthStateChanged((user)=>{
+      setUser(user)
+    })
+  })
   return (
    <Fragment>
       <BrowserRouter>
       <Nav/>
       <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={user ? <Navigate to={"/hero"}/> : <Login />} />
             <Route path="/hero" element={
                <Fragment>
                     <Hero />  
